29c72961ebc7e7efcc519b70c02185f472959eb4
Abhijeet1520 <freelancer.abhijeet.kumar@gmail.com>
2020-12-02 15:29:11 +0530

    Fix: use .env to hide API keys and import auth

--- src/context/firebase.js
+++ src/context/firebase.js
@@ -1,35 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/auth';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-/*
-import dotenv from 'dotenv';
-import env from "react-dotenv";
-var firebaseConfig = {
-    apiKey: dotenv.env.API_KEY,
-    authDomain: dotenv.env.AUTH_DOMAIN,
-    databaseURL: dotenv.env.DATABASE_URL,
-    projectId: dotenv.env.PROJECT_ID,
-    storageBucket: dotenv.env.STORAGE_BUCKET,
-    messagingSenderId: dotenv.env.MESSAGING_SENDER_ID,
-    appId: dotenv.env.APP_ID,
-    measurementId: dotenv.env.MEASUREMENT_ID
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    databaseURL: process.env.REACT_APP_databaseURL,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
-*/
-
-var firebaseConfig = {
-    apiKey: "AIzaSyAfQRm2lZK0GuU03ozXD66ajX-IMeWWJHU",
-    authDomain: "movienest-aed57.firebaseapp.com",
-    databaseURL: "https://movienest-aed57.firebaseio.com",
-    projectId: "movienest-aed57",
-    storageBucket: "movienest-aed57.appspot.com",
-    messagingSenderId: "216306749242",
-    appId: "1:216306749242:web:ae01f22dcb0cd503dcca03",
-    measurementId: "G-0J4BH800WQ"
-};
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
