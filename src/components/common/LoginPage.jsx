d678f9138bb3031e408a43eb2685e4e2c26bee90
Abhijeet1520 <freelancer.abhijeet.kumar@gmail.com>
2020-12-02 15:44:09 +0530

    Fix: commented auth function block to complete the build

--- src/components/common/LoginPage.jsx
+++ src/components/common/LoginPage.jsx
@@ -12,45 +12,46 @@ const LoginPage = ({modalType}) => {
     const [errorShow, setErrorShow] = useState('');
     const [alertShow, setAlertShow] = useState(false);
     
-    const handleAuth = () => {
-        var actionCodeSettings = {
-            url: 'https://www.voting-site.netlify.com/adminPanel',
-        };
-        firebase.auth().sendSignInLinkToEmail(emailField, actionCodeSettings).then(response => {
-            console.log(response);
-        })
-        // Confirm the link is a sign-in with email link.
-        if (firebase.auth().isSignInWithEmailLink(window.location.href)) {
-            // Additional state parameters can also be passed via URL.
-            // This can be used to continue the user's intended action before triggering
-            // the sign-in operation.
-            // Get the email if available. This should be available if the user completes
-            // the flow on the same device where they started it.
-            var email = window.localStorage.getItem('emailForSignIn');
-            if (!email) {
-                // User opened the link on a different device. To prevent session fixation
-                // attacks, ask the user to provide the associated email again. For example:
-                email = emailField;
-            }
-            // The client SDK will parse the code from the link for you.
-            firebase.auth().signInWithEmailLink(email, window.location.href)
-            .then(function(result) {
-                console.log(result)
-                // Clear email from storage.
-                window.localStorage.removeItem('emailForSignIn');
-                // You can access the new user via result.user
-                // Additional user info profile not available via:
-                // result.additionalUserInfo.profile == null
-                // You can check if the user is new or existing:
-                // result.additionalUserInfo.isNewUser
-            })
-            .catch(function(error) {
-                setErrorShow(error.message);
-                // Some error occurred, you can inspect the code: error.code
-                // Common errors could be invalid email and invalid or expired OTPs.
-            });
-        }
-    }
+    // const handleAuth = () => {
+    //     var actionCodeSettings = {
+    //         url: 'https://www.voting-site.netlify.com/adminPanel',
+    //     };
+    //     firebase.auth().sendSignInLinkToEmail(emailField, actionCodeSettings).then(response => {
+    //         console.log(response);
+    //     })
+    //     // Confirm the link is a sign-in with email link.
+    //     if (firebase.auth().isSignInWithEmailLink(window.location.href)) {
+    //         // Additional state parameters can also be passed via URL.
+    //         // This can be used to continue the user's intended action before triggering
+    //         // the sign-in operation.
+    //         // Get the email if available. This should be available if the user completes
+    //         // the flow on the same device where they started it.
+    //         var email = window.localStorage.getItem('emailForSignIn');
+    //         if (!email) {
+    //             // User opened the link on a different device. To prevent session fixation
+    //             // attacks, ask the user to provide the associated email again. For example:
+    //             email = emailField;
+    //         }
+    //         // The client SDK will parse the code from the link for you.
+    //         firebase.auth().signInWithEmailLink(email, window.location.href)
+    //         .then(function(result) {
+    //             console.log(result)
+    //             // Clear email from storage.
+    //             window.localStorage.removeItem('emailForSignIn');
+    //             // You can access the new user via result.user
+    //             // Additional user info profile not available via:
+    //             // result.additionalUserInfo.profile == null
+    //             // You can check if the user is new or existing:
+    //             // result.additionalUserInfo.isNewUser
+    //         })
+    //         .catch(function(error) {
+    //             setErrorShow(error.message);
+    //             // Some error occurred, you can inspect the code: error.code
+    //             // Common errors could be invalid email and invalid or expired OTPs.
+    //         });
+    //     }
+    // }
+    
     const handleInputChange = (e) => {
         e.persist();
         setEmailField(e.target.value);
